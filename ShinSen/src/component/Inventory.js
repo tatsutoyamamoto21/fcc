dcff0c9dad2305fafb653aed0eb52c06c05348c6
Tatsuto Yamamoto <ty1021@ic.ac.uk>
2023-08-07 00:58:15 +0100

    Add Inventory.

---
+++ ShinSen/src/component/Inventory.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Inventory = () => {
+  return (
+    <View>
+      <Text>
+        Hello World
+      </Text>
+    </View>
+  )
+}
+
+export default Inventory
