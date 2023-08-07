20e829a39cc535e7e6e712aaca01761eaabb4001
Tatsuto Yamamoto <ty1021@ic.ac.uk>
2023-08-07 01:41:30 +0100

    Updated Inventory.js

--- ShinSen/src/component/Inventory.js
+++ ShinSen/src/component/Inventory.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const Inventory = () => {
   return (
-    <View>
-      <Text>
-        Hello World Dev
-        Hello World Tats
-      </Text>
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <View>
+        <Text>
+          Hello World Dev
+          Hello World Tats
+        </Text>
+      </View>
+    </SafeAreaView>
+
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+})
 
 export default Inventory;
