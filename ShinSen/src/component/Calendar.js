5ec2a3afcded6aff6c581db506563c3184348a71
Devamitra Acharya <acharyadevamitra@gmail.com>
2023-08-07 01:05:23 +0100

    Added Calendar.js

---
+++ ShinSen/src/component/Calendar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Calendar = () => {
+  return (
+    <View>
+      <Text>
+        Hello World This is Calendar
+      </Text>
+    </View>
+  )
+}
+
+export default Calendar
