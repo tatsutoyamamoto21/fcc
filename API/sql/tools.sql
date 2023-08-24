9be6a8ff36a8c38da00582f1092f90de1ef779f4
Devamitra Acharya <acharyadevamitra@gmail.com>
2023-08-24 04:42:27 +0100

    minor fix API

--- API/sql/tools.sql
+++ API/sql/tools.sql
@@ -1,4 +1,5 @@
 drop database `MyDB`;
 use MyDB;
 select * from fridge_1;
-select * from fridge_1 order by 'ExpDate';
+select * from fridge_1 order by ExpDate;
+INSERT INTO fridge_1 (ItemName, ExpDate, IsBestBefore, Portion) VALUES ('add items', '2023-08-22 00:00:00', 0, 1);
